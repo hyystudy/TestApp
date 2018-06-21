2fbf609febae6144be6ab9b88c5e694331c45de1
贺洋洋 <768061025@qq.com>
2018-06-21 13:57:18 +0800

    Initial commit

---
+++ testlibrary/src/main/java/com/ldzc/testlibrary/Utils.java
@@ -0,0 +1,14 @@
+package com.ldzc.testlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2018/6/21.
+ */
+
+public class Utils {
+    public static final String TAG = "TEST";
+    public static void logd(String msg) {
+        Log.d(TAG, msg);
+    }
+}
